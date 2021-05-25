431a5bf31a544f5a31eac6d88a15a3e72bb2ff3d
Lily Habberfield <lilyhabbs@gmail.com>
2021-05-25 02:41:09 +0000

    Calculate total & avg sales, and high sales stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_sales = Store.sum(:annual_revenue)
+@avg_sales = Store.average(:annual_revenue)
+@high_sales = Store.where('annual_revenue >= 1000000').count
+
+puts "Total revenue of all stores: $#{@total_sales}"
+puts "Average revenue of all stores: $#{@avg_sales}"
+puts "# stores generating $1M or more in sales: #{@high_sales}"
